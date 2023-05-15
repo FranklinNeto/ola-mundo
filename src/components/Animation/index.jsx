be4fc21501d175697e268dd92e955b328216e752
Franklin Ferreira Rezende Neto <2frneto@gmail.com>
2023-05-15 12:11:01 -0400

    fix: animation duration

--- src/components/Animation/index.jsx
+++ src/components/Animation/index.jsx
@@ -6,7 +6,7 @@ function Animation({ children }) {
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
-      transition={{ duration: 0.2 }}
+      transition={{ duration: 0.3 }}
     >
       {children}
     </motion.div>
